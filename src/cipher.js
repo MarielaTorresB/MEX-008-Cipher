b3ea365ab41a47451c145d3de14d05c4efcba42d
MarielaTorresB <martz_maru@hotmail.com>
2019-06-19 02:24:52 -0500

    Añadiendo codificación y decodificación de minúsculas

--- src/cipher.js
+++ src/cipher.js
@@ -1,33 +1,43 @@
 window.cipher = {
   encode : (offset, string) => {
-
       const despl =parseInt(offset);
       const size= string.length;
       let textocifrado= "";
       let i=0;
       while(i<size){
         let x=string.charCodeAt(i);
-        let y=((x-65+despl)%26+65);
-        textocifrado = textocifrado + String.fromCharCode(y);
-        i++;
+        if(65 <= x && x <= 90){
+          let y=((x-65+despl)%26+65);
+          textocifrado = textocifrado + String.fromCharCode(y);
+          i++;
+        } else if (97 <= x && x <= 122) {
+          let y=((x-97+despl)%26+97);
+          textocifrado = textocifrado + String.fromCharCode(y);
+          i++;
         }
+      }
       return textocifrado;
 
 },
   decode : (offset, string) => {
-
     const despl = parseInt(offset);
     const size=string.length;
     let textodescifrado="";
     let i=0;
+    let y;
     while(i<size){
       let x=string.charCodeAt(i);
-      let y= (x+65-despl)%26+65;
-      //let y=(x-(despl%26));
-      textodescifrado=textodescifrado + String.fromCharCode(y);
-      i++;
-  }
-  return textodescifrado;
+      if(65 <= x && x <= 90){
+        let y=((x+65-despl)%26+65);
+        textodescifrado = textodescifrado + String.fromCharCode(y);
+        i++;
+        } else if (97 <= x && x <= 122) {
+          let y=((x+26-despl-97)%26+97);
+          textodescifrado = textodescifrado + String.fromCharCode(y);
+          i++;
+        }
+}
+    return textodescifrado;
 },
   //   encode : (offset, string) => {
   //   const n =parseInt(offset);
